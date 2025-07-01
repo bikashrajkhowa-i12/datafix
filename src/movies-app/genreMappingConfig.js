bfdc0a46dcd801aacd975bce5ddf3168dd6552cf
bikashrajkhowa-i12 <bikashrajkhowa424265@gmail.com>
2025-07-01 19:14:15 +0530

    feat: :zap: updated genre config creation

--- src/movies-app/genreMappingConfig.js
+++ src/movies-app/genreMappingConfig.js
@@ -7,20 +7,32 @@ const genreMappingConfig = async () => {
   try {
     const database = "movies_database";
     const collection = "configs";
+
+    const toIdNameMap = (list) =>
+      list.reduce((acc, curr) => {
+        acc[curr.id] = curr.name;
+        return acc;
+      }, {});
+
     const configObj = {
       name: "GENRE_MAPPING_CONFIG",
       description: "This config has genre-ids mapped to genre-names",
       configs: {
-        movie: movieGenres,
-        tv_show: tvGenres,
+        movie: toIdNameMap(movieGenres),
+        tv_show: toIdNameMap(tvGenres),
       },
+      created_by: "bikashrajkhowa",
       created_at: new Date(),
       updated_at: new Date(),
     };
+
     await createConfig(database, collection, configObj);
+    console.log("Genre config inserted successfully!");
   } catch (error) {
+    console.error("Error inserting genre config:", error);
     throw error;
   }
 };
+
 
 module.exports = genreMappingConfig;
